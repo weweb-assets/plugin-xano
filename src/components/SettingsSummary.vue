cc44054e52a87adb6fe3a3b7c456fd742f19a052
Dams-d <damien.duro8@gmail.com>
2022-05-02 18:56:28 +0200

    Fix settings

--- src/components/SettingsSummary.vue
+++ src/components/SettingsSummary.vue
@@ -1,18 +1,45 @@
 <template>
-    <div class="flex items-center">
-        <wwEditorIcon name="key" class="mr-2"></wwEditorIcon>
-        <span class="caption-m">{{ settings.privateData.apiKey }}</span>
-    </div>
-    <div class="flex items-center">
+    <div class="flex items-center caption-m">
         <wwEditorIcon name="link" class="mr-2"></wwEditorIcon>
-        <span class="caption-m">{{ settings.privateData.instanceId }}</span>
+        {{ instance }}
     </div>
+    <wwLoader :loading="isLoading" />
 </template>
 
 <script>
 export default {
     props: {
+        plugin: { type: Object, required: true },
         settings: { type: Object, required: true },
+    },
+    emits: ['update:settings'],
+    data() {
+        return {
+            isLoading: false,
+            instances: null,
+        };
+    },
+    computed: {
+        instance() {
+            const instance = this.instances.find(instance => instance.id === this.settings.privateData.instanceId);
+            return instance ? instance.display : 'Not found';
+        },
+    },
+    mounted() {
+        this.fetchInstances(this.settings.privateData.apiKey);
+    },
+    methods: {
+        async fetchInstances(apiKey) {
+            if (!apiKey) return;
+            try {
+                this.isLoading = true;
+                this.instances = await this.plugin.fetchInstances(apiKey);
+            } catch (err) {
+                wwLib.wwLog.error(err);
+            } finally {
+                this.isLoading = false;
+            }
+        },
     },
 };
 </script>
