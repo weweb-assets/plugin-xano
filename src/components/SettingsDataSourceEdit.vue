f74e427727180298d9ff86e8f93f3c04fca6ae35
Alexis JC <alexisjc.dev@gmail.com>
2023-07-31 19:18:57 +0200

    chore: update style

--- src/components/SettingsDataSourceEdit.vue
+++ src/components/SettingsDataSourceEdit.vue
@@ -1,11 +1,11 @@
 <template>
-    <p>
+    <p class="label">
         X-Data-Source Header
         <a class="xano-settings-edit__link" href="https://docs.xano.com/database/data-sources" target="_blank">
             Learn more
         </a>
     </p>
-    <p class="mb-3 label-3">
+    <p class="mb-3 label-sm">
         This will only affect Dynamic collections. Static and Cached collections will always use the Production value.
     </p>
     <wwEditorInputRow
@@ -32,79 +32,66 @@
         @update:modelValue="updatePublicSettings('xDataSourceEditor', $event)"
         small
     />
-    <Expandable class="ww-dropdown__tree-children" :active="showHeaders" @toggle="showHeaders = !showHeaders">
-        <template #header>
-            <wwEditorIcon class="ww-dropdown__header-icon" name="chevron-right" small />
-            <div class="ml-1 label-1">Shared Headers</div>
+    <p class="label mt-3">Custom Headers</p>
+    <p class="mb-3 label-sm">
+        Global headers will be used for every request to your Xano made browser side. Secure headers will be used for
+        accessing your data when generating static pages on our side.
+    </p>
+    <wwEditorInputRow
+        label="Global headers"
+        type="array"
+        :model-value="settings.publicData.globalHeaders"
+        bindable
+        @update:modelValue="updatePublicSettings('globalHeaders', $event)"
+        @add-item="updatePublicSettings('globalHeaders', [...(settings.publicData.globalHeaders || []), {}])"
+    >
+        <template #default="{ item, setItem }">
+            <wwEditorInputRow
+                type="query"
+                :model-value="item.key"
+                label="Key"
+                placeholder="Enter a value"
+                small
+                bindable
+                @update:modelValue="setItem({ ...item, key: $event })"
+            />
+            <wwEditorInputRow
+                type="query"
+                :model-value="item.value"
+                label="Value"
+                placeholder="Enter a value"
+                small
+                bindable
+                @update:modelValue="setItem({ ...item, value: $event })"
+            />
         </template>
-        <template #content>
-            <div class="ml-2">
-                <p class="mb-3 label-sm">
-                    Global headers will be used for every request to your Xano made browser side. Secure headers will be
-                    used for accessing your data when generating static pages on our side.
-                </p>
-                <wwEditorInputRow
-                    label="Global headers"
-                    type="array"
-                    :model-value="settings.publicData.globalHeaders"
-                    bindable
-                    @update:modelValue="updatePublicSettings('globalHeaders', $event)"
-                    @add-item="
-                        updatePublicSettings('globalHeaders', [...(settings.publicData.globalHeaders || []), {}])
-                    "
-                >
-                    <template #default="{ item, setItem }">
-                        <wwEditorInputRow
-                            type="query"
-                            :model-value="item.key"
-                            label="Key"
-                            placeholder="Enter a value"
-                            small
-                            bindable
-                            @update:modelValue="setItem({ ...item, key: $event })"
-                        />
-                        <wwEditorInputRow
-                            type="query"
-                            :model-value="item.value"
-                            label="Value"
-                            placeholder="Enter a value"
-                            small
-                            bindable
-                            @update:modelValue="setItem({ ...item, value: $event })"
-                        />
-                    </template>
-                </wwEditorInputRow>
-                <wwEditorInputRow
-                    label="Secure headers"
-                    type="array"
-                    :model-value="settings.privateData.secureHeaders"
-                    @update:modelValue="updatePrivateSettings('secureHeaders', $event)"
-                    @add-item="
-                        updatePrivateSettings('secureHeaders', [...(settings.privateData.secureHeaders || []), {}])
-                    "
-                >
-                    <template #default="{ item, setItem }">
-                        <wwEditorInputRow
-                            type="query"
-                            :model-value="item.key"
-                            label="Key"
-                            placeholder="Enter a value"
-                            small
-                            @update:modelValue="setItem({ ...item, key: $event })"
-                        />
-                        <wwEditorInputRow
-                            type="query"
-                            :model-value="item.value"
-                            label="Value"
-                            placeholder="Enter a value"
-                            small
-                            @update:modelValue="setItem({ ...item, value: $event })"
-                        />
-                    </template>
-                </wwEditorInputRow>
-            </div>
+    </wwEditorInputRow>
+    <wwEditorInputRow
+        label="Secure headers"
+        type="array"
+        :model-value="settings.privateData.secureHeaders"
+        @update:modelValue="updatePrivateSettings('secureHeaders', $event)"
+        @add-item="updatePrivateSettings('secureHeaders', [...(settings.privateData.secureHeaders || []), {}])"
+    >
+        <template #default="{ item, setItem }">
+            <wwEditorInputRow
+                type="query"
+                :model-value="item.key"
+                label="Key"
+                placeholder="Enter a value"
+                small
+                @update:modelValue="setItem({ ...item, key: $event })"
+            />
+            <wwEditorInputRow
+                type="query"
+                :model-value="item.value"
+                label="Value"
+                placeholder="Enter a value"
+                small
+                @update:modelValue="setItem({ ...item, value: $event })"
+            />
         </template>
-    </Expandable>
+    </wwEditorInputRow>
 </template>
 
 <script>
