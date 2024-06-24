2a1d29a899d1f404511d337796a06b9602cc0906
Alexis JC <alexisjc.dev@gmail.com>
2024-06-24 10:37:32 +0200

    fix: fetchCollection

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -58,7 +58,7 @@ export default {
     /*=============================================m_ÔÔ_m=============================================\
         Collection API
     \================================================================================================*/
-    async fetchCollection(collection) {
+    async _fetchCollection(collection) {
         if (collection.mode === 'dynamic') {
             try {
                 const { data } = await this.request(collection.config);
