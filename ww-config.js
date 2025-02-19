cbea6e5acd454b6c966decd1b7181645dc91427a
Alexis JC <alexisjc.dev@gmail.com>
2025-02-19 15:07:32 +0100

    fix: formula exemple

--- ww-config.js
+++ ww-config.js
@@ -334,7 +334,7 @@ export default {
                     body: {
                         type: 'object',
                         description:
-                            'Request body data. Only used for non-GET requests. Should be a JSON-serializable object. The key values are bindable, but not the whole object. The object cannot be bind, you have to bind individual sub keys. eg. {email: <bind-email>, password: <bind-password>}',
+                            'Request body data. Only used for non-GET requests. Should be a JSON-serializable object. The key values are bindable, but not the whole object. The object cannot be bind, you have to bind individual sub keys. eg. {email: {__wwType: "...", code: "..."}, password:  {__wwType: "...", code: "..."}}',
                         bindable: false,
                     },
                     dataType: {
